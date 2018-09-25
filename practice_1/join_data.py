63c1ca134346180aa47b3d82d1d9809a5640f7b8
egjimenezg <egjimenezg@gmail.com>
2018-09-24 23:23:03 -0500

    Adding script that joins the data

---
+++ practice_1/join_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+fileData = pd.DataFrame()
+data = []
+
+for i in range(1,5):
+  dataFrame = pd.read_csv("StatusData" + str(i) + ".csv", index_col=None, header=0)
+  data.append(dataFrame)
+
+fileData = pd.concat(data)
+
+fileData.to_csv("StatusDataComplete.csv")
